f0a23390dcd1f69bc7c72d43469a53b9e03943a3
ether <etherzhhb@gmail.com>
2010-06-15 20:11:42 +0800

    Add ScheduleDriver pass.

---
+++ lib/VerilogBackend/ScheduleDriver.h
@@ -0,0 +1,24 @@
+//===-------- ScheduleDriver.h - The Scheduler driver pass  ------*- C++ -*-===//
+//
+//                            The Verilog Backend
+//
+// Copyright: 2010 by Hongbin Zheng. all rights reserved.
+// IMPORTANT: This software is supplied to you by Hongbin Zheng in consideration
+// of your agreement to the following terms, and your use, installation, 
+// modification or redistribution of this software constitutes acceptance
+// of these terms.  If you do not agree with these terms, please do not use, 
+// install, modify or redistribute this software. You may not redistribute, 
+// install copy or modify this software without written permission from 
+// Hongbin Zheng. 
+//
+//===----------------------------------------------------------------------===//
+//
+// This file define the ScheduleDriver Pass, which run difference schedulers on
+// a llvm function to schedule the Hardware atoms.
+//
+//===----------------------------------------------------------------------===//
+//
+#ifndef VBE_SCHEDULE_DRIVER_H
+#define VBE_SCHEDULE_DRIVER_H
+
+#endif
