09ffa58ec8fb4c2075683628e4474ba7384c7963
ether <etherzhhb@gmail.com>
2010-09-03 15:35:06 +0800

    Refactor: Seperate the scheduler and scheduling pass.

--- lib/VerilogBackend/ForceDirectedScheduler.cpp
+++ lib/VerilogBackend/ForceDirectedScheduler.cpp
@@ -33,17 +33,29 @@ using namespace esyn;
 
 namespace {
 struct fds_sort {
-  ForceDirectedInfo *Info;
-  fds_sort(ForceDirectedInfo *s) : Info(s) {}
+  ForceDirectedInfo &Info;
+  fds_sort(ForceDirectedInfo &s) : Info(s) {}
   bool operator() (const HWAOpFU *LHS, const HWAOpFU *RHS) const;
 };
 
 struct FDSPass : public BasicBlockPass {
+  /// @name Common pass interface
+  //{
+  static char ID;
+  FDSPass() : BasicBlockPass(&ID) {}
+  bool runOnBasicBlock(BasicBlock &BB);
+  void getAnalysisUsage(AnalysisUsage &AU) const;
+  void print(raw_ostream &O, const Module *M) const;
+  //}
+};
+
+class FDSBase {
+protected:
   HWAtomInfo *HI;
-  ForceDirectedInfo *FDInfo;
+  ForceDirectedInfo FDInfo;
   FSMState *CurState;
 
-  unsigned MII;
+  unsigned II;
 
   /// @name PriorityQueue
   //{
@@ -60,27 +72,18 @@ struct FDSPass : public BasicBlockPass {
   // scheduled
   bool scheduleCriticalPath();
 
+  unsigned findBestStep(HWAtom *A);
+
   bool scheduleAtom(HWAtom *A);
   bool scheduleQueue(AtomQueueType &Queue);
   bool scheduleAtII();
   //}
 
-  unsigned findBestStep(HWAtom *A);
-
-  void clear();
-
+public:
+  FDSBase(HWAtomInfo *HAInfo, FSMState *S, unsigned MII)
+    : HI(HAInfo), FDInfo(HAInfo, S), CurState(S), II(MII) {}
   void FDListSchedule();
   void FDModuloSchedule();
-
-  /// @name Common pass interface
-  //{
-  static char ID;
-  FDSPass() : BasicBlockPass(&ID) {}
-  bool runOnBasicBlock(BasicBlock &BB);
-  void releaseMemory();
-  void getAnalysisUsage(AnalysisUsage &AU) const;
-  void print(raw_ostream &O, const Module *M) const;
-  //}
 };
 } //end namespace
 
@@ -94,9 +97,9 @@ bool fds_sort::operator()(const HWAOpFU* LHS, const HWAOpFU* RHS) const {
     return false;
 
   // Schedule the low mobility nodes first.
-  if (Info->getTimeFrame(LHS) > Info->getTimeFrame(RHS))
+  if (Info.getTimeFrame(LHS) > Info.getTimeFrame(RHS))
     return true; // Place RHS first.
-  else if (Info->getTimeFrame(LHS) < Info->getTimeFrame(RHS))
+  else if (Info.getTimeFrame(LHS) < Info.getTimeFrame(RHS))
     return false;
 
   //unsigned LHSLatency = FDS->getASAPStep(LHS);
@@ -117,9 +120,114 @@ void FDSPass::getAnalysisUsage(AnalysisUsage &AU) const {
   AU.setPreservesAll();
 }
 
+bool FDSPass::runOnBasicBlock(BasicBlock &BB) {
+  DEBUG(dbgs() << "==================== " << BB.getName() << '\n');
+  HWAtomInfo *HI = &getAnalysis<HWAtomInfo>();
+
+  FSMState *CurState = HI->getStateFor(BB);
+
+  // Create the FDInfo.
+  ModuloScheduleInfo MSInfo(HI, &getAnalysis<LoopInfo>(), CurState);
+  
+  FDSBase fds(HI, CurState, MSInfo.computeMII());
+
+  if (MSInfo.isModuloSchedulable()) {
+    fds.FDModuloSchedule();
+  } else
+    fds.FDListSchedule();
+
+  HI->setTotalCycle(CurState->getEndSlot() + 1);
+
+  // Do not forget to schedule the delay atom;
+  for (FSMState::iterator I = CurState->begin(), E = CurState->end();
+       I != E; ++I) {
+    HWAtom *A = *I;
+    assert(A->isScheduled() && "Schedule incomplete!");
+  }
+
+  return false;
+}
+
+bool FDSBase::scheduleAtII() {
+  fds_sort s(FDInfo);
+  AtomQueueType AQueue(s);
+
+  // Schedule other nodes.
+  AQueue.clear();
+  fillQueue(AQueue, CurState->begin(), CurState->end());
+
+  return scheduleQueue(AQueue);
+}
+
+void FDSBase::FDModuloSchedule() {
+  unsigned StartStep = HI->getTotalCycle();
+  CurState->scheduledTo(StartStep);
+
+  // Ensure us can schedule the critical path.
+  for (;;) {
+    FDInfo.buildFDInfo();
+    if (scheduleCriticalPath())
+      break;
+    DEBUG(FDInfo.dumpTimeFrame());
+    // TODO: check if we could ever schedule these node without breaking the
+    // resource constrain by check the DG.
+    // If the resource average DG is bigger than the total available resource
+    // we can never schedule the nodes without breaking the resource constrain.
+    CurState->resetSchedule(StartStep);
+    FDInfo.lengthenCriticalPath();
+  }
+
+  // Dirty Hack: Search the solution by increasing MII and critical path
+  // alternatively.
+
+  FDInfo.setMII(II);
+  for (;;) {
+    FDInfo.buildFDInfo();
+    if (scheduleCriticalPath())
+      break;
+    DEBUG(FDInfo.dumpTimeFrame());
+    FDInfo.lengthenMII();
+    CurState->resetSchedule(StartStep);
+  }
+
+  bool lastIncMII = true;
+  for (;;) {
+    bool SchedSucc = scheduleCriticalPath();
+    assert(SchedSucc
+      && "Why nodes critical path can not be schedule since DG is ok?");
+
+    if (scheduleAtII()) {
+      DEBUG(FDInfo.dumpTimeFrame());
+      DEBUG(FDInfo.dumpDG());
+      // Set up the initial interval.
+      CurState->setII(FDInfo.getMII());
+      break;
+    } else if (lastIncMII) {
+      FDInfo.lengthenCriticalPath();
+      lastIncMII = false;
+    } else {
+      FDInfo.lengthenMII();
+      II = FDInfo.getMII();
+      lastIncMII = true;
+    }
+    // Prepare for next schedule.
+    CurState->resetSchedule(StartStep);
+    FDInfo.buildFDInfo();
+  }
+
+  for (FSMState::iterator I = CurState->begin(), E = CurState->end();
+      I != E; ++I) {
+    HWAtom *A = *I;
+    if (A->isScheduled())
+      continue;
+    bool res = scheduleAtom(A);
+    assert(res && "Why A can not schedule?");
+  }
+}
+
 template<class It>
-void FDSPass::fillQueue(AtomQueueType &Queue, It begin, It end,
-                             HWAtom *FirstNode) {
+void FDSBase::fillQueue(AtomQueueType &Queue, It begin, It end,
+                        HWAtom *FirstNode) {
   for (It I = begin, E = end; I != E; ++I) {
     HWAtom *A = *I;
 
@@ -134,111 +242,11 @@ void FDSPass::fillQueue(AtomQueueType &Queue, It begin, It end,
   Queue.reheapify();
 }
 
-bool FDSPass::runOnBasicBlock(BasicBlock &BB) {
-  DEBUG(dbgs() << "==================== " << BB.getName() << '\n');
-  HI = &getAnalysis<HWAtomInfo>();
-
-  CurState = HI->getStateFor(BB);
-
-  // Create the FDInfo.
-  FDInfo = new ForceDirectedInfo(HI, CurState);
-  ModuloScheduleInfo MSInfo(HI, &getAnalysis<LoopInfo>(), CurState);
-
-  if (MSInfo.isModuloSchedulable()) {
-    MII = MSInfo.computeMII();
-    FDModuloSchedule();
-  } else
-    FDListSchedule();
-
-  HI->setTotalCycle(CurState->getEndSlot() + 1);
-
-  // Do not forget to schedule the delay atom;
-  for (FSMState::iterator I = CurState->begin(), E = CurState->end();
-       I != E; ++I) {
-    HWAtom *A = *I;
-    if (!A->isScheduled()) {
-      A->scheduledTo(FDInfo->getASAPStep(A));
-    }
-  }
-
-  delete FDInfo;
-
-  return false;
-}
-
-void FDSPass::FDModuloSchedule() {
-  unsigned StartStep = HI->getTotalCycle();
-  CurState->scheduledTo(StartStep);
-
-  // Ensure us can schedule the critical path.
-  for (;;) {
-    FDInfo->buildFDInfo();
-    if (scheduleCriticalPath())
-      break;
-    DEBUG(FDInfo->dumpTimeFrame());
-    // TODO: check if we could ever schedule these node without breaking the
-    // resource constrain by check the DG.
-    // If the resource average DG is bigger than the total available resource
-    // we can never schedule the nodes without breaking the resource constrain.
-    CurState->resetSchedule(StartStep);
-    FDInfo->lengthenCriticalPath();
-  }
-
-  // Dirty Hack: Search the solution by increasing MII and critical path
-  // alternatively.
-
-  FDInfo->setMII(MII);
-  for (;;) {
-    FDInfo->buildFDInfo();
-    if (scheduleCriticalPath())
-      break;
-    DEBUG(FDInfo->dumpTimeFrame());
-    FDInfo->lengthenMII();
-    CurState->resetSchedule(StartStep);
-  }
-
-  bool lastIncMII = true;
-  for (;;) {
-    bool SchedSucc = scheduleCriticalPath();
-    assert(SchedSucc
-      && "Why nodes critical path can not be schedule since DG is ok?");
-
-    if (scheduleAtII()) {
-      DEBUG(FDInfo->dumpTimeFrame());
-      DEBUG(FDInfo->dumpDG());
-      // Set up the initial interval.
-      CurState->setII(FDInfo->getMII());
-      return;
-    } else if (lastIncMII) {
-      FDInfo->lengthenCriticalPath();
-      lastIncMII = false;
-    } else {
-      FDInfo->lengthenMII();
-      MII = FDInfo->getMII();
-      lastIncMII = true;
-    }
-    // Prepare for next schedule.
-    CurState->resetSchedule(StartStep);
-    FDInfo->buildFDInfo();
-  }
-}
-
-bool FDSPass::scheduleAtII() {
-  fds_sort s(FDInfo);
-  AtomQueueType AQueue(s);
-
-  // Schedule other nodes.
-  AQueue.clear();
-  fillQueue(AQueue, CurState->begin(), CurState->end());
-
-  return scheduleQueue(AQueue);
-}
-
-void FDSPass::FDListSchedule() {
+void FDSBase::FDListSchedule() {
   unsigned StartStep = HI->getTotalCycle();
   for(;;) {
     CurState->resetSchedule(StartStep);
-    FDInfo->buildFDInfo();
+    FDInfo.buildFDInfo();
 
     fds_sort s(FDInfo);
     AtomQueueType AQueue(s);
@@ -246,7 +254,7 @@ void FDSPass::FDListSchedule() {
     fillQueue(AQueue, CurState->begin(), CurState->end());
 
     if (!scheduleQueue(AQueue))
-      FDInfo->lengthenCriticalPath();
+      FDInfo.lengthenCriticalPath();
     else // Break the loop if we schedule successful.
       break;
   }
@@ -254,29 +262,39 @@ void FDSPass::FDListSchedule() {
   // control logic for loop if MS is disable.
   if (CurState->haveSelfLoop())
     CurState->setII(CurState->getTotalSlot());
-  DEBUG(FDInfo->dumpTimeFrame());
+  DEBUG(FDInfo.dumpTimeFrame());
+
+  for (FSMState::iterator I = CurState->begin(), E = CurState->end();
+      I != E; ++I) {
+    HWAtom *A = *I;
+    if (A->isScheduled())
+      continue;
+    
+    bool res = scheduleAtom(A);
+    assert(res && "Why A can not schedule?");
+  }
 }
 
 
-unsigned FDSPass::findBestStep(HWAtom *A) {
+unsigned FDSBase::findBestStep(HWAtom *A) {
   std::pair<unsigned, double> BestStep = std::make_pair(0, 1e32);
   DEBUG(dbgs() << "\tScan for best step:\n");
   // For each possible step:
-  for (unsigned i = FDInfo->getASAPStep(A), e = FDInfo->getALAPStep(A) + 1;
+  for (unsigned i = FDInfo.getASAPStep(A), e = FDInfo.getALAPStep(A) + 1;
       i != e; ++i) {
     DEBUG(dbgs() << "At Step " << i << "\n");
 
     // Temporary schedule A to i so we can get a more accurate pred and succ
     // force. Because the back edge constraint from A will be considered.
     A->scheduledTo(i);
-    FDInfo->buildTimeFrame();
+    FDInfo.buildTimeFrame();
     // Compute the forces.
-    double SelfForce = FDInfo->computeSelfForceAt(A, i);
+    double SelfForce = FDInfo.computeSelfForceAt(A, i);
     // The follow function will invalid the time frame.
     DEBUG(dbgs() << " Self Force: " << SelfForce);
-    double PredForce = FDInfo->computePredForceAt(A, i);
+    double PredForce = FDInfo.computePredForceAt(A, i);
     DEBUG(dbgs() << " Pred Force: " << PredForce);
-    double SuccForce = FDInfo->computeSuccForceAt(A, i);
+    double SuccForce = FDInfo.computeSuccForceAt(A, i);
     DEBUG(dbgs() << " Succ Force: " << SuccForce);
     double Force = SelfForce + PredForce + SuccForce;
     DEBUG(dbgs() << " Force: " << Force);
@@ -288,26 +306,18 @@ unsigned FDSPass::findBestStep(HWAtom *A) {
   return BestStep.first;
 }
 
-void FDSPass::releaseMemory() {
-  clear();
-}
-
-void FDSPass::clear() {
-  CurState = 0;
-}
-
 void FDSPass::print(raw_ostream &O, const Module *M) const { }
 
-bool FDSPass::scheduleCriticalPath() {
+bool FDSBase::scheduleCriticalPath() {
   bool AnyScheduled = false;
   for (FSMState::iterator I = CurState->begin(), E = CurState->end();
       I != E; ++I) {
     HWAtom *A = *I;
   
-    if (A->isScheduled() || FDInfo->getTimeFrame(A) != 1)
+    if (A->isScheduled() || FDInfo.getTimeFrame(A) != 1)
       continue;
 
-    unsigned step = FDInfo->getASAPStep(A);
+    unsigned step = FDInfo.getASAPStep(A);
     DEBUG(A->print(dbgs()));
     DEBUG(dbgs() << " asap step: " << step << "\n");
     A->scheduledTo(step);
@@ -316,18 +326,18 @@ bool FDSPass::scheduleCriticalPath() {
 
   // Time frame may changed because of backedge constraints of scheduled nodes.
   if (AnyScheduled) {
-    FDInfo->buildFDInfo();
-    return FDInfo->isResourceConstraintPreserved();
+    FDInfo.buildFDInfo();
+    return FDInfo.isResourceConstraintPreserved();
   }
   // Else the time frame not changed.
   return true;
 }
 
-bool FDSPass::scheduleAtom(HWAtom *A) {
+bool FDSBase::scheduleAtom(HWAtom *A) {
   assert(!A->isScheduled() && "A already scheduled!");
   DEBUG(A->print(dbgs()));
-  unsigned step = FDInfo->getASAPStep(A);
-  if (FDInfo->getTimeFrame(A) > 1) {
+  unsigned step = FDInfo.getASAPStep(A);
+  if (FDInfo.getTimeFrame(A) > 1) {
     step = findBestStep(A);
     DEBUG(dbgs() << "\n\nbest step: " << step << "\n");
     // If we can not schedule A.
@@ -338,11 +348,11 @@ bool FDSPass::scheduleAtom(HWAtom *A) {
   }
 
   A->scheduledTo(step);
-  FDInfo->buildFDInfo();
+  FDInfo.buildFDInfo();
   return scheduleCriticalPath();
 }
 
-bool FDSPass::scheduleQueue(AtomQueueType &Queue) {
+bool FDSBase::scheduleQueue(AtomQueueType &Queue) {
   while (!Queue.empty()) {
     // TODO: Short the list
     HWAOpFU *A = Queue.top();
