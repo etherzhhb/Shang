119c666bd8d8420ebca4800d6f69fff9ff638727
ether <etherzhhb@gmail.com>
2010-12-06 19:20:57 +0800

    Add the parition information to allow users to specific partition information.

---
+++ include/vtm/PartitionInfo.h
@@ -0,0 +1,58 @@
+//===--------- vtm/PartitionInfo.h - SW/HW parition infomation ------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file define the PartitionInfo class, which provide the information about
+// software/hardware partition.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef VTM_PARTITION_INFO_H
+#define VTM_PARTITION_INFO_H
+
+#include "llvm/Function.h"
+#include "llvm/ADT/StringSet.h"
+
+
+namespace llvm {
+struct VTargetMachine;
+
+class PartitionInfo {
+
+  // DO NOT IMPLEMENT
+  PartitionInfo(const PartitionInfo&);
+  // DO NOT IMPLEMENT
+  const PartitionInfo &operator=(const PartitionInfo&);
+
+  StringSet<> HWFunctions;
+
+  void setHardware(const std::string &S) {
+    HWFunctions.insert(S);
+  }
+
+  friend struct VTargetMachine;
+public:
+  PartitionInfo() {}
+
+  bool isHardware(const Function &F) const {
+    // If no explicit partition available, all function are synthesis to
+    //  hardware by default.
+    if (HWFunctions.empty())
+      return true;
+    
+    return HWFunctions.count(F.getName());
+  }
+
+  bool empty() const { return HWFunctions.empty(); }
+};
+
+PartitionInfo &partition();
+
+}
+
+#endif
---
+++ lib/TargetCfgs/PartitionInfo.cpp
@@ -0,0 +1,22 @@
+//===----------- PartitionInfo.cpp - SW/HW parition infomation ------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file inplement the PartitionInfo class, which provide the information
+// about software/hardware partition.
+//
+//===----------------------------------------------------------------------===//
+#include "vtm/PartitionInfo.h"
+
+using namespace llvm;
+
+PartitionInfo &llvm::partition() {
+  static PartitionInfo Info;
+
+  return Info;
+}
