68dd52352df80e3f02680cef25b2038ff39a7b88
ether <etherzhhb@gmail.com>
2011-04-08 15:35:49 +0800

    Add comment for register layout in a rtl module.

--- lib/RTLBackend/RTLWriter.cpp
+++ lib/RTLBackend/RTLWriter.cpp
@@ -459,6 +459,14 @@ void RTLWriter::emitAllocatedFUs() {
 }
 
 void RTLWriter::emitAllRegister() {
+  // We organize the registers in a module like 64 bits width ram, and we treat
+  // the register number as the address of the register in the 'ram'. The
+  // address of the register is always aligned with the register's size in byte,
+  // that is, the register number of a 16 bits register will always divisible by
+  // 2. So we can address the register as if it is in the ram. for example, if
+  // we have a 32 bit register whose number is 20, that means it located at the
+  // high part of the 2th 64 bits physics register.
+
   // Emit the register with max word length.
   for (VFuncInfo::phyreg_iterator I = FuncInfo->phyreg_begin(8),
        E = FuncInfo->phyreg_end(8); I < E; ++I)
