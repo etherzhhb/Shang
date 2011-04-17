7b92b5fee14dd0aa949cf51107de5ef444fe7886
ether <etherzhhb@gmail.com>
2011-04-17 21:41:05 +0800

    Add wrapper class for Intrinsic Instructions.

--- VIntrinsicsInfo.cpp
+++ VIntrinsicsInfo.cpp
@@ -17,10 +17,11 @@
 #include "llvm/Function.h"
 #include "llvm/Module.h"
 #include "llvm/Type.h"
-#include "llvm/CodeGen/ValueTypes.h "
+#include "llvm/CodeGen/ValueTypes.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/ADT/StringExtras.h"
+#include "llvm/ADT/StringMap.h"
 #include <cstring>
 
 namespace llvm {
@@ -32,7 +33,20 @@ namespace llvm {
 }
 
 using namespace llvm;
+//===----------------------------------------------------------------------===//
+static inline unsigned lookupNameHelper(const char *Name, unsigned Len) {
+    if (Len < 5 || Name[4] != '.' || Name[0] != 'l' || Name[1] != 'l'
+        || Name[2] != 'v' || Name[3] != 'm')
+      return 0;  // All intrinsics start with 'llvm.'
 
+#define GET_FUNCTION_RECOGNIZER
+#include "VGenIntrinsics.inc"
+#undef GET_FUNCTION_RECOGNIZER
+    return 0;
+}
+
+//===----------------------------------------------------------------------===//
+// IntrinsicInfo Implementation.
 std::string VIntrinsicInfo::getName(unsigned IntrID, const Type **Tys,
                                     unsigned numTys) const {
     static const char *const names[] = {
@@ -61,18 +75,10 @@ std::string VIntrinsicInfo::getName(unsigned IntrID, const Type **Tys,
 }
 
 unsigned VIntrinsicInfo::lookupName(const char *Name, unsigned Len) const {
-    if (Len < 5 || Name[4] != '.' || Name[0] != 'l' || Name[1] != 'l'
-      || Name[2] != 'v' || Name[3] != 'm')
-      return 0;  // All intrinsics start with 'llvm.'
-
-#define GET_FUNCTION_RECOGNIZER
-#include "VGenIntrinsics.inc"
-#undef GET_FUNCTION_RECOGNIZER
-    return 0;
+  return lookupNameHelper(Name, Len);
 }
 
-unsigned VIntrinsicInfo::
-  lookupGCCName(const char *Name) const {
+unsigned VIntrinsicInfo::lookupGCCName(const char *Name) const {
     return vtmIntrinsic::getIntrinsicForGCCBuiltin("vtm",Name);
 }
 
@@ -116,3 +122,62 @@ Function *VIntrinsicInfo::getDeclaration(Module *M, unsigned IntrID,
                            getType(M->getContext(), IntrID, Tys, numTys),
                            AList));
 }
+
+
+//===----------------------------------------------------------------------===//
+static unsigned getIntVal(const Value *V) {
+  return cast<ConstantInt>(V)->getZExtValue();
+}
+
+//===----------------------------------------------------------------------===//
+// Intrinsic Instructions Implementation.
+vtmIntrinsic::ID VIntrinsicInst::getIntrinsicID() const {
+  if (const Function *CF = getCalledFunction())
+    if (const ValueName *ValName = CF->getValueName())
+      return (vtmIntrinsic::ID)lookupNameHelper(ValName->getKeyData(),
+                                              ValName->getKeyLength());
+
+  return (vtmIntrinsic::ID)0;
+}
+
+bool VIntrinsicInst::classof(const CallInst *I) {
+  if (const Function *CF = I->getCalledFunction())
+    if (const ValueName *ValName = CF->getValueName())
+      return
+        lookupNameHelper(ValName->getKeyData(), ValName->getKeyLength()) != 0;
+
+  return false;
+}
+
+unsigned VAllocaBRamInst::getNumElement() const {
+  return getIntVal(getArgOperand(0));
+}
+
+unsigned VAllocaBRamInst::getElementSizeInBytes() const {
+  return  getIntVal(getArgOperand(1));
+}
+
+unsigned VAllocaBRamInst::getBRamId() const {
+  return cast<PointerType>(getType())->getAddressSpace();
+}
+
+Value *VAccessBRamInst::getPointerOperand() const {
+  return getArgOperand(0);
+}
+
+Value *VAccessBRamInst::getValueOperand() const {
+  return getArgOperand(1);
+}
+
+bool VAccessBRamInst::isStore() const {
+  return getIntVal(getArgOperand(2));
+}
+
+unsigned VAccessBRamInst::getAlignment() const {
+  return getIntVal(getArgOperand(3));
+}
+
+bool VAccessBRamInst::isVolatile() const {
+  return getIntVal(getArgOperand(4));
+}
+
--- VIntrinsicsInfo.h
+++ VIntrinsicsInfo.h
@@ -14,6 +14,7 @@
 #define VTM_INTRINSICS_H
 
 #include "llvm/Intrinsics.h"
+#include "llvm/IntrinsicInst.h"
 #include "llvm/Target/TargetIntrinsicInfo.h"
 
 namespace llvm {
@@ -26,15 +27,73 @@ namespace llvm {
       , num_vtm_intrinsics
     };
   }
-  // TODO: IntrinsicInst for VTM.
+
+  /// VIntrinsicInst - A useful wrapper class for inspecting calls to intrinsic
+  /// functions.  This allows the standard isa/dyncast/cast functionality to
+  /// work with calls to intrinsic functions.  
+  class VIntrinsicInst : public CallInst {
+    VIntrinsicInst();                      // DO NOT IMPLEMENT
+    VIntrinsicInst(const VIntrinsicInst&);  // DO NOT IMPLEMENT
+    void operator=(const VIntrinsicInst&); // DO NOT IMPLEMENT
+  public:
+    /// getIntrinsicID - Return the intrinsic ID of this intrinsic.
+    ///
+    vtmIntrinsic::ID getIntrinsicID() const;
+
+    // Methods for support type inquiry through isa, cast, and dyn_cast:
+    static inline bool classof(const VIntrinsicInst *) { return true; }
+    static bool classof(const CallInst *I);
+
+    static inline bool classof(const Value *V) {
+      return isa<CallInst>(V) && classof(cast<CallInst>(V));
+    }
+  };
+
+  /// VAllocaBramInst - This represents the llvm.vtm.alloca.bram instruction.
+  ///
+  class VAllocaBRamInst : public VIntrinsicInst {
+  public:
+    // Methods for support type inquiry through isa, cast, and dyn_cast:
+    static inline bool classof(const VAllocaBRamInst *) { return true; }
+    static inline bool classof(const VIntrinsicInst *I) {
+      return I->getIntrinsicID() == vtmIntrinsic::vtm_alloca_bram;
+    }
+    static inline bool classof(const Value *V) {
+      return isa<VIntrinsicInst>(V) && classof(cast<VIntrinsicInst>(V));
+    }
+
+    unsigned getBRamId() const;
+    unsigned getNumElement() const;
+    unsigned getElementSizeInBytes() const;
+  };
+
+  /// VAccessBramInst - This represents the llvm.vtm.access.bram instruction.
+  ///
+  class VAccessBRamInst : public VIntrinsicInst {
+  public:
+    // Methods for support type inquiry through isa, cast, and dyn_cast:
+    static inline bool classof(const VAllocaBRamInst *) { return true; }
+    static inline bool classof(const VIntrinsicInst *I) {
+      return I->getIntrinsicID() == vtmIntrinsic::vtm_access_bram;
+    }
+    static inline bool classof(const Value *V) {
+      return isa<VIntrinsicInst>(V) && classof(cast<VIntrinsicInst>(V));
+    }
+
+    Value *getPointerOperand() const;
+    Value *getValueOperand() const;
+    bool isStore() const;
+    unsigned getAlignment() const;
+    bool isVolatile() const;
+  };
 
   class VIntrinsicInfo : public TargetIntrinsicInfo {
   public:
     std::string getName(unsigned IntrID, const Type **Tys = 0,
                         unsigned numTys = 0) const;
-  
+
     unsigned lookupName(const char *Name, unsigned Len) const;
-  
+
     unsigned lookupGCCName(const char *Name) const;
   
     bool isOverloaded(unsigned IID) const;
