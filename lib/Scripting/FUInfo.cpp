96e0371edf74bb568e219417d48a8a6256d20721
yyl <yyl@WIN-TR18CRLTVPV.(none)>
2012-02-22 11:25:18 +0800

    modify the compute of the MuxCost

--- lib/Scripting/FUInfo.cpp
+++ lib/Scripting/FUInfo.cpp
@@ -53,7 +53,10 @@ namespace llvm {
 
     // Default area cost parameter.
     unsigned LUTCost = 64;
-    unsigned RegCost[64] ;
+    unsigned RegCost[64] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
+                            21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,
+                            38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,
+                            55,56,57,58,59,60,61,62,63,64};
 
     ////////////////////////////////////
     unsigned AddCost[64] ;
@@ -61,8 +64,8 @@ namespace llvm {
     unsigned ShiftCost[64] ;
     unsigned ICmpCost[64] ;
     //////////////////////////////////
-    unsigned MuxSizeCost = 96;
-    unsigned MUXCost = 64;
+    //unsigned MuxSizeCost = 96;
+    unsigned MUXCost[9] = {0, 0, 65, 88, 131, 259, 324, 364, 454};
     unsigned MaxLutSize = 4;
     unsigned MaxMuxPerLut = 4;
     unsigned MaxAllowedMuxSize = 8;
