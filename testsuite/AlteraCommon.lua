b65e2a2881d2d23cc1ed28914367850a65eca051
ether <etherzhhb@gmail.com>
2012-06-13 11:39:24 +0800

    Use multicylce_path instead of max_delay.

--- testsuite/AlteraCommon.lua
+++ testsuite/AlteraCommon.lua
@@ -2,11 +2,11 @@ RunOnDatapath = [=[
 #local Slack = RTLDatapath.Slack
 #local DstName = RTLDatapath.Nodes[1].Name
 #local SrcName = RTLDatapath.Nodes[table.getn(RTLDatapath.Nodes)].Name
-#_put('#' .. DstName .. '<-' .. SrcName .. ' Slack ' .. Slack .. ' Path nodes: ')
-#for i, n in pairs(RTLDatapath.Nodes) do
-#  _put(n.Name .. ', ')
-#end
 
+#if (RTLDatapath.isCriticalPath == 1) then
+$(_put('#')) $(DstName) <- $(SrcName) Slack $(Slack)
+set_multicycle_path -from $(SrcName)* -to $(DstName)* -setup -end $(Slack)
+#end
 #if Functions[FuncInfo.Name] == nil then
 #  DstName = '*' .. CurModule:getName() .. '_inst|' .. DstName
 #  SrcName = '*' .. CurModule:getName() .. '_inst|' .. SrcName
@@ -15,21 +15,20 @@ RunOnDatapath = [=[
 #for i, n in pairs(RTLDatapath.Nodes) do
 #  if n.Name ~= 'n/a' and n.Name ~= DstName and n.Name ~= SrcName then
 #    local ThuName = n.Name
+#    has_thu_node = true
 #    if Functions[FuncInfo.Name] == nil then
 #      ThuName = '*' .. CurModule:getName() .. '_inst|' .. ThuName
 #    end
-set_max_delay -from $(SrcName)* -through $(ThuName)* -to $(DstName)* $(Slack * PERIOD)ns
+$(_put('#')) $(DstName) <- $(ThuName) <- $(SrcName) Slack $(Slack)
+set_multicycle_path -from $(SrcName)* -through $(ThuName)* -to $(DstName)* -setup -end $(Slack)
 #  end -- End valid thu node.
 #end -- end for
-
-#if (Slack > 1 and RTLDatapath.isCriticalPath == 1) then
-  set_max_delay -from $src -to $dst $(Slack * PERIOD)ns
-#end
 ]=]
 
 SDCHeader = [=[
 create_clock -name "clk" -period $(PERIOD)ns [get_ports {clk}]
 derive_pll_clocks -create_base_clocks
+set_multicycle_path -from [get_clocks {clk}] -to [get_clocks {clk}] -hold -end 1
 ]=]
 
 Misc.DatapathScript = [=[
