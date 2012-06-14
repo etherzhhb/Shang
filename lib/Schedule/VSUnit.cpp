35dcacb05f06ed4e9b63493ceec9edf2eeaf6e06
ether <etherzhhb@gmail.com>
2012-06-14 15:36:34 +0800

    Bug fix VSUnit printing.
    Do not print the trace for pseudo intructions, which do not have such
    operand.

--- lib/Schedule/VSUnit.cpp
+++ lib/Schedule/VSUnit.cpp
@@ -444,8 +444,9 @@ void VSUnit::print(raw_ostream &OS) const {
     if (MachineInstr *Instr = getInstrAt(i)) {
       const TargetInstrInfo *TII = Instr->getParent()->getParent()->getTarget()
                                          .getInstrInfo();
-      OS << TII->getName(Instr->getDesc().getOpcode()) << ' '
-         << *VInstrInfo::getTraceOperand(Instr);
+      OS << TII->getName(Instr->getDesc().getOpcode()) << ' ';
+      if (!Instr->isPseudo())
+        OS << *VInstrInfo::getTraceOperand(Instr);
       if (i) OS << ' ' << int(getLatencyAt(i));
       OS << '\n';
       DEBUG(OS << *Instr << '\n');
