5736f3ac91e977ac920dcfb937a9c070b503682a
ether <etherzhhb@gmail.com>
2012-08-12 18:01:51 +0800

    Estimate the FU cost at IR level by simply build a data-path net-list and accumulate the cost of FU in data-path.

--- lib/RTLCodegen/DesignMetrics.cpp
+++ lib/RTLCodegen/DesignMetrics.cpp
@@ -22,6 +22,9 @@
 #include "llvm/Pass.h"
 #include "llvm/Target/TargetData.h"
 #include "llvm/Support/InstIterator.h"
+#include "llvm/ADT/DenseSet.h"
+#define DEBUG_TYPE "vtm-design-metrics"
+#include "llvm/Support/Debug.h"
 
 using namespace llvm;
 
@@ -54,6 +57,10 @@ class DesignMetrics : public EarlyDatapathBuilderContext {
   DatapathContainer DPContainer;
   EarlyDatapathBuilder Builder;
 
+  // The data-path value which are used by control-path operations.
+  typedef std::set<VASTValue*> ValSetTy;
+  ValSetTy LiveOutedVal;
+
   // TODO: Model the control-path, in the control-path, we can focus on the MUX
   // in the control-path, note that the effect of FU allocation&binding
   // algorithm should also be considered when estimating resource usage.
@@ -72,13 +79,17 @@ class DesignMetrics : public EarlyDatapathBuilderContext {
 
   VASTValPtr getAsOperand(Value *Op, bool GetAsInlineOperand);
 
+  // Visit the expression tree whose root is Root and return the cost of the
+  // tree, insert all visited data-path nodes into Visited.
+  uint64_t getExprTreeFUCost(VASTValPtr Root, ValSetTy &Visited) const;
+  uint64_t getFUCost(VASTValue *V) const;
 public:
   DesignMetrics(TargetData *TD) : Builder(*this, TD) {}
 
-  void addInstruction(Instruction &Inst) {
-    if (VASTValPtr V = Builder.visit(Inst))
-      Builder.indexVASTExpr(&Inst, V);
-  }
+  void addInstruction(Instruction &Inst);
+
+  // Visit all data-path expression and compute the cost.
+  uint64_t getDatapathFUCost() const;
 };
 
 struct DesignMetricsPass : public FunctionPass {
@@ -121,6 +132,93 @@ VASTValPtr DesignMetrics::getAsOperand(Value *Op, bool GetAsInlineOperand) {
   return ValueOp;
 }
 
+void DesignMetrics::addInstruction(Instruction &Inst) {
+  if (VASTValPtr V = Builder.visit(Inst)) {
+    Builder.indexVASTExpr(&Inst, V);
+    return;
+  }
+
+  // Else Inst is a control-path instruction, all its operand are live-outed.
+  // A live-outed data-path expression and its children should never be
+  // eliminated.
+  typedef Instruction::op_iterator op_iterator;
+  for (op_iterator I = Inst.op_begin(), E = Inst.op_end(); I != E; ++I) {
+    if (VASTValPtr V = Builder.lookupExpr(*I))
+      LiveOutedVal.insert(V.get());
+  }
+}
+
+uint64_t DesignMetrics::getFUCost(VASTValue *V) const {
+  VASTExpr *Expr = dyn_cast<VASTExpr>(V);
+  // We can only estimate the cost of VASTExpr.
+  if (!Expr) return 0;
+
+  unsigned ValueSize = std::min(V->getBitWidth(), 64u);
+
+  switch (Expr->getOpcode()) {
+  default: break;
+
+  case VASTExpr::dpAdd: return VFUs::AddCost[ValueSize];
+  case VASTExpr::dpMul: return VFUs::MulCost[ValueSize];
+  case VASTExpr::dpShl:
+  case VASTExpr::dpSRA:
+  case VASTExpr::dpSRL: return VFUs::ShiftCost[ValueSize];
+  }
+
+  return 0;
+}
+
+uint64_t DesignMetrics::getExprTreeFUCost(VASTValPtr Root, ValSetTy &Visited)
+                                          const {
+  uint64_t Cost = 0;
+  // FIXME: Provide a generic depth-first search iterator for VASTValue tree.
+  typedef VASTValue::dp_dep_it ChildIt;
+  std::vector<std::pair<VASTValue*, ChildIt> > WorkStack;
+
+  WorkStack.push_back(std::make_pair(Root.get(),
+                                     VASTValue::dp_dep_begin(Root.get())));
+
+  Cost += getFUCost(Root.get());
+  
+  while (!WorkStack.empty()) {
+    VASTValue *N;
+    ChildIt It;
+    tie(N, It) = WorkStack.back();
+
+    // All children are visited.  
+    if (It == VASTValue::dp_dep_end(N)) {
+      WorkStack.pop_back();
+      continue;
+    }
+
+    // Depth first traverse the child of current node.
+    VASTValue *ChildNode = It->get().get();
+    ++WorkStack.back().second;
+
+    // Had we visited this node?
+    if (!Visited.insert(ChildNode).second) continue;
+
+    // Visit the node and compute its FU cost.
+    Cost += getFUCost(ChildNode);
+
+    WorkStack.push_back(std::make_pair(ChildNode,
+                                       VASTValue::dp_dep_begin(ChildNode)));
+  }
+
+  return Cost;
+}
+
+uint64_t DesignMetrics::getDatapathFUCost() const {
+  uint64_t Cost = 0;
+  ValSetTy Visited;
+
+  typedef ValSetTy::const_iterator iterator;
+  for (iterator I = LiveOutedVal.begin(), E = LiveOutedVal.end(); I != E; ++I)
+    Cost += getExprTreeFUCost(*I, Visited);
+
+  return Cost;
+}
+
 char DesignMetricsPass::ID = 0;
 
 bool DesignMetricsPass::runOnFunction(Function &F) {
@@ -128,6 +226,9 @@ bool DesignMetricsPass::runOnFunction(Function &F) {
 
   for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I)
     Metrics.addInstruction(*I);
+
+  DEBUG(dbgs() << "Data-path cost of function " << F.getName() << ':'
+               << Metrics.getDatapathFUCost() << '\n');
   
   return false;
 }
