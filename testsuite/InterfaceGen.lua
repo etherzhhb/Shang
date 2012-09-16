257313763e7e89cd2f48e764751642f73df6a408
ether <etherzhhb@gmail.com>
2012-09-16 16:51:53 +0800

    Move the ModelSim script template to a standalone file.

--- testsuite/InterfaceGen.lua
+++ testsuite/InterfaceGen.lua
@@ -364,27 +364,3 @@ local _, message = preprocess {input=DUTtbGen, output=tbFile}
 if message ~= nil then print(message) end
 tbFile:close()
 ]=]}
-
-ModelsimScGen = [=[
-$('#')!/bin/sh
-PATH=~/altera/10.1/modelsim_ase/bin/:~/altera/modelsim_ase/bin/:$PATH
-export DISPLAY=:0
-vlib work
-vlog +define+quartus_synthesis -sv $(RTLModuleName).v
-vlog -sv INTF_$(RTLModuleName).v
-vlog -sv DUT_TOP_tb.v
-vlog -sv BRAM.sv
-vsim DUT_TOP_tb -novopt -do "run -all;quit -f"
-
-]=]
-
-Passes.ModelsimScGen = { FunctionScript = [=[
-if Functions[FuncInfo.Name] ~= nil then
-end
-]=], GlobalScript =[=[
-local ModelDoFile = assert(io.open (MODELDOFILE, "w+"))
-local preprocess = require "luapp" . preprocess
-local _, message = preprocess {input=ModelsimScGen, output=ModelDoFile}
-if message ~= nil then print(message) end
-ModelDoFile:close()
-]=]}
---
+++ testsuite/ModelSimGen.lua
@@ -0,0 +1,22 @@
+ModelsimGenTemplate = [=[
+$('#')!/bin/sh
+PATH=~/altera/10.1/modelsim_ase/bin/:~/altera/modelsim_ase/bin/:$PATH
+vlib work
+vlog +define+quartus_synthesis -sv $(RTLModuleName).v
+vlog -sv INTF_$(RTLModuleName).v
+vlog -sv DUT_TOP_tb.v
+vlog -sv BRAM.sv
+vsim -t 1ps work.DUT_TOP_tb -c -do "run -all;vcd flush;quit -f"
+
+]=]
+
+Passes.ModelsimGen = { FunctionScript = [=[
+if Functions[FuncInfo.Name] ~= nil then
+end
+]=], GlobalScript =[=[
+local ModelDoFile = assert(io.open (MODELDOFILE, "w+"))
+local preprocess = require "luapp" . preprocess
+local _, message = preprocess {input=ModelsimGenTemplate, output=ModelDoFile}
+if message ~= nil then print(message) end
+ModelDoFile:close()
+]=]}
